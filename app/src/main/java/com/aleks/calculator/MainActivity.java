fa9fce5ebbdef7ad25f8b1dabbcffd40670e9f0e
aleks <alekstatter@gmail.com>
2016-04-13 02:00:45 +0300

    Shortened the code, the beginning of moving calculator brain to a different project

--- app/src/main/java/com/aleks/calculator/MainActivity.java
+++ app/src/main/java/com/aleks/calculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -19,23 +20,6 @@ public class MainActivity extends AppCompatActivity {
     private CalculatorEngine calculator = new CalculatorEngine();
 
     private TextView textViewAnswer;
-    private Button btnZero;
-    private Button btnOne;
-    private Button btnTwo;
-    private Button btnThree;
-    private Button btnFour;
-    private Button btnFive;
-    private Button btnSix;
-    private Button btnSeven;
-    private Button btnEight;
-    private Button btnNine;
-    private Button btnPlus;
-    private Button btnMinus;
-    private Button btnMultiply;
-    private Button btnDivide;
-    private Button btnComma;
-    private Button btnClear;
-    private Button btnEqual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, "onCreate called");
         }
         if(savedInstanceState != null) {
-            if(BuildConfig.DEBUG) {
+            if (BuildConfig.DEBUG) {
                 Log.e(TAG, "onSavedInstanceState onCreate called");
             }
             calculator.setCurrentNumber(savedInstanceState.getString(STATE_CALCULATION));
@@ -57,162 +41,9 @@ public class MainActivity extends AppCompatActivity {
         //Ühendan nupud äpiga
         textViewAnswer = (TextView) findViewById(R.id.nrfield);
         textViewAnswer.setText("");
-        btnZero = (Button) findViewById(R.id.btn16);
-        btnOne = (Button) findViewById(R.id.btn11);
-        btnTwo = (Button) findViewById(R.id.btn12);
-        btnThree = (Button) findViewById(R.id.btn13);
-        btnFour = (Button) findViewById(R.id.btn07);
-        btnFive = (Button) findViewById(R.id.btn08);
-        btnSix = (Button) findViewById(R.id.btn09);
-        btnSeven = (Button) findViewById(R.id.btn03);
-        btnEight = (Button) findViewById(R.id.btn04);
-        btnNine = (Button) findViewById(R.id.btn05);
-        btnPlus = (Button) findViewById(R.id.btn10);
-        btnMinus = (Button) findViewById(R.id.btn14);
-        btnMultiply = (Button) findViewById(R.id.btn06);
-        btnDivide = (Button) findViewById(R.id.btn02);
-        btnComma = (Button) findViewById(R.id.btn15);
-        btnEqual = (Button) findViewById(R.id.btn17);
-        btnClear = (Button) findViewById(R.id.btn01);
-
-        //Nupp "1" OnClick
-        btnOne.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.insert(1);
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "2" OnClick
-        btnTwo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.insert(2);
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "3" OnClick
-        btnThree.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.insert(3);
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "4" OnClick
-        btnFour.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.insert(4);
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "5" OnClick
-        btnFive.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.insert(5);
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "6" OnClick
-        btnSix.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.insert(6);
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "7" OnClick
-        btnSeven.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.insert(7);
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "8" OnClick
-        btnEight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.insert(8);
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "9" OnClick
-        btnNine.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.insert(9);
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "0" OnClick
-        btnZero.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.insert(0);
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "+" OnClick
-        btnPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.operation("+");
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "-" OnClick
-        btnMinus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.operation("-");
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "*" OnClick
-        btnMultiply.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.operation("*");
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "/" OnClick
-        btnDivide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.operation("/");
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "=" OnClick
-        btnEqual.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.calculation();
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "Comma" OnClick
-        btnComma.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.comma();
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
-        //Nupp "Clear" OnClick
-        btnClear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculator.clear();
-                textViewAnswer.setText(calculator.getCurrentNumber());
-            }
-        });
         drawCurrentCalculator();
     }
+
 
     public void drawCurrentCalculator() {
         textViewAnswer.setText("" + calculator.getCurrentNumber());
@@ -230,51 +61,34 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedIntanceState);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if(BuildConfig.DEBUG) {
-            Log.e(TAG, "onStart called");
-        }
+    public void nrOnclick(View view) {
+        Button btn = (Button) view;
+        //See on vajutatud nupu tekst
+        int nr = Integer.parseInt(btn.getText().toString());
+        calculator.insert(nr);
+        textViewAnswer.setText(calculator.getCurrentNumber());
     }
 
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        if(BuildConfig.DEBUG) {
-            Log.e(TAG, "onRestart called");
-        }
+    public void opOnclick(View view) {
+        Button btn = (Button) view;
+        //See on vajutatud nupu tekst
+        String op = btn.getText().toString();
+        calculator.operation(op);
+        textViewAnswer.setText(calculator.getCurrentNumber());
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if(BuildConfig.DEBUG) {
-            Log.e(TAG, "onResume called");
-        }
+    public void resetOnclick(View view) {
+        calculator.clear();
+        textViewAnswer.setText(calculator.getCurrentNumber());
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if(BuildConfig.DEBUG) {
-            Log.e(TAG, "onPause called");
-        }
+    public void commaOnclick(View view) {
+        calculator.comma();
+        textViewAnswer.setText(calculator.getCurrentNumber());
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if(BuildConfig.DEBUG) {
-            Log.e(TAG, "onStop called");
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if(BuildConfig.DEBUG) {
-            Log.e(TAG, "onDestroy called");
-        }
+    public void equalOnclick(View view) {
+        calculator.calculation();
+        textViewAnswer.setText(calculator.getCurrentNumber());
     }
 }
